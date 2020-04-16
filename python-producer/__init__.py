42d076527614dc62302ae2ac1926c92f9da39039
Martin Maroši <marvusm.mmi@gmail.com>
2020-04-16 09:26:16 +0200

    Setup basic flask API for producing kafka messages.

--- python-producer/__init__.py
+++ python-producer/__init__.py
@@ -1,8 +1,30 @@
 from kafka import KafkaProducer
+import flask
+from flask import request
+import json
+
+# create kafka producer
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-print(producer)
-for _ in range(100):
-  producer.send('foobar', b'hola')
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
-producer.flush()
+@app.route('/', methods=['GET'])
+def home():
+  return "<h1>Test server for kafka producer</h1>"
+
+@app.route('/send-message', methods=["POST"])
+def send_message():
+  data = json.loads(request.data)
+  print(data)
+  producer.send('test', request.data)
+  return request.data
+
+
+app.run()
+
+#print(producer)
+#for _ in range(100):
+#  
+
+#producer.flush()
